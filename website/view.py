8af4de601f30f3ad2a2e774c823f3d5801ffe79a
JinZhang <2636423765@qq.com>
2019-04-29 11:04:11 +0800

    refactor:修改数据传入有误问题

--- website/view.py
+++ website/view.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render;
 
 def home(request):
-    return render(request, "home.html", [
-        {
-            "name" : "工具名称",
-            "category" : "工具类别",
-            "tkey" : "ac6116b2555f703585a8b7abca84b1bf",
-            "description" : "工具描述",
-            "downloadCount" : 0,
-            "score" : 0.0,
-            "author" : "作者",
-            "uploadTime" : "2019-04-29",
-        },
-    ]);
+    return render(request, "home.html", {
+        "toolInfoList" : [
+            {
+                "name" : "工具名称",
+                "category" : "工具类别",
+                "tkey" : "ac6116b2555f703585a8b7abca84b1bf",
+                "description" : "工具描述",
+                "downloadCount" : 0,
+                "score" : 0.0,
+                "author" : "作者",
+                "uploadTime" : "2019-04-29",
+            },
+        ],
+    });
