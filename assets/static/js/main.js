a06c63f339bb6b787dff1a7e5db03fc27712cf83
JinZhang <2636423765@qq.com>
2019-08-01 18:57:14 +0800

    feat:新增重置密码功能

--- assets/static/js/main.js
+++ assets/static/js/main.js
@@ -32,7 +32,7 @@ $(function(){
 				<div class='row'>\
 					<div class='dialog-background col-md-4 col-md-offset-4'>\
 						<a id='closeDialogPage' href='javascript:void(0);' title='关闭弹窗'><span class='glyphicon glyphicon-remove'></span>关闭</a>\
-						" + content + "\
+						<div class='dialog-content'>" + content + "</div>\
 					</div>\
 				</div>\
 			</div>\
@@ -51,8 +51,12 @@ $(function(){
 		// 更新弹窗页尺寸方法
 		function updateDialogPageSize(){
 			if ($('#dialogPage').length > 0) {
-				$('#dialogPage').width(Math.max($(window).width(), $("body").width()));
-				$('#dialogPage').height(Math.max($(window).height(), $("body").height()));
+				var pageWidth = Math.max($(window).width(), $("body").width());
+				$('#dialogPage').width(pageWidth);
+				var pageHeight = Math.max($(window).height(), $("body").height());
+				$('#dialogPage').height(pageHeight);
+				// 移动到弹窗中心
+				$('body,html').animate({scrollLeft : (pageWidth - $(window).width())/2, scrollTop: (pageHeight - $(window).height())/2},280);
 			}
 		}
 		updateDialogPageSize();
@@ -84,20 +88,23 @@ $(function(){
 			}
 		}, 1000);
 		// 创建弹窗
-		createDialog("<div class='dialog-content text-center'>"+ content +"<p class='dialog-interval'><span id='timeCountDown'>"+ seconds +"</span>&nbsp;秒后自动关闭</p></div>", function(){
+		createDialog("<div class='text-center'>"+ content +"<p class='dialog-interval'><span id='timeCountDown'>"+ seconds +"</span>&nbsp;秒后自动关闭</p></div>", function(){
 			window.clearInterval(intervalId);
 		});
 	}
 	// 创建登录弹窗
 	function createLoginDialog(){
 		// 创建弹窗
-		createDialogPage("<form class='login-form dialog-content' role='form'>\
+		createDialogPage("<form class='login-form' role='form'>\
 						<h2>登陆PyToolsIP</h2>\
 						<input id='loginUserName' class='form-control' type='text' placeholder='用户名' required autofocus />\
 						<input id='loginPassword' class='form-control' type='password' placeholder='密码' required />\
 						<label id='loginRemember'><input type='checkbox'>&nbsp;记住用户</label>\
 						<button id='loginButton' class='btn btn-lg btn-success btn-block' type='button'><span class='glyphicon glyphicon-log-in'></span>&nbsp;登陆</button>\
-						<label><a id='registerDialog' href='javascript:void(0)'>注册</a></label>\
+						<div class='login-link clearfix'>\
+							<a id='registerDialog' class='pull-left' href='javascript:void(0)'>注册用户</a>\
+							<a id='forgetDialog' class='pull-right' href='javascript:void(0)'>重置密码</a>\
+						</div>\
 					</form>");
 		// 绑定登陆按钮的点击事件
 		$("#loginButton").on("click",function(){
@@ -126,11 +133,15 @@ $(function(){
 		$("#registerDialog").on("click",function(){
 			createRegisterDialog();
 		});
+		// 绑定更新密码超链接的点击事件
+		$("#forgetDialog").on("click",function(){
+			createForgetDialog();
+		});
 	}
 	// 创建登出弹窗
 	function createLogoutDialog(data){
 		// 创建弹窗
-		createDialogPage("<div class='dialog-content text-center'>\
+		createDialogPage("<div class='text-center'>\
 							<h2>玩家信息</h2>\
 							<p>用户名：<span>" + data.name + "</span></p>\
 							<p>邮箱：<span>" + data.email + "</span></p>\
@@ -200,7 +211,7 @@ $(function(){
 	// 创建注册弹窗
 	function createRegisterDialog(){
 		// 创建弹窗
-		createDialogPage("<form id='registerForm' class='login-form dialog-content' role='form' enctype='multipart/form-data'>\
+		createDialogPage("<form id='registerForm' class='login-form' role='form' enctype='multipart/form-data'>\
 						<h2>PyToolsIP用户注册</h2>\
 						<input name='name' class='form-control' type='text' placeholder='用户名' required autofocus />\
 						<input id='password' name='password' class='form-control' type='password' placeholder='密码' required />\
@@ -274,6 +285,80 @@ $(function(){
 						}
 						// 创建登录成功的弹窗
 						createIntervalDialog("<h2>注册成功!</h2>", 2);
+					} else {
+						alert("注册失败！")
+					}
+				});
+            }
+       });
+	}
+	// 创建更新密码弹窗
+	function createForgetDialog(){
+		// 创建弹窗
+		createDialogPage("<form id='forgetForm' class='login-form' role='form' enctype='multipart/form-data'>\
+						<h2>PyToolsIP更新密码</h2>\
+						<input id='password' name='password' class='form-control' type='password' placeholder='新密码' required />\
+						<input id='verifyPwd' name='verifyPwd' class='form-control' type='password' placeholder='确认密码' required />\
+						<input name='email' class='form-control' type='text' placeholder='注册邮箱' required />\
+						<div class='input-group'>\
+							<input name='verifyCode' class='form-control' type='text' placeholder='验证码' required />\
+							<span class='input-group-btn'>\
+								<button id='verifyCodeBtn' class='btn btn-default' type='button'>获取验证码</button>\
+							</span>\
+						</div>\
+						<button class='btn btn-lg btn-success btn-block' type='submit'><span class='glyphicon glyphicon-registration-mark'></span>&nbsp;更新密码</button>\
+					</form>");
+		// 绑定登陆按钮的点击事件
+		$("#forgetForm").validate({
+            rules: {
+                password: {
+                    required: true,
+                    rangelength: [6, 16],
+                },
+                verifyPwd: {
+                    required: true,
+                    rangelength: [6, 16],
+                    equalTo: "#password"
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                verifyCode: {
+                	required: true,
+                }
+            },
+            messages: {
+                password: {
+                    required: "请输入密码",
+                    rangelength: $.validator.format("请输入介于{0}和{1}长度的值"),
+                },
+                verifyPwd: {
+                    required: "请输入确认密码",
+                    rangelength: $.validator.format("请输入介于{0}和{1}长度的值"),
+                    equalTo: "确认密码和密码不匹配"
+                },
+                email: {
+                    required: "请输入邮箱",
+                    email: "邮箱格式有误",
+                },
+                verifyCode: {
+                	required: "请输入验证码",
+                }
+            },
+            submitHandler: function() {
+                $.post(registerUrl, {
+					password : $("#registerForm input[name='password']").val(),
+					email : $("#registerForm input[name='email']").val(),
+					verifyCode : $("#registerForm input[name='verifyCode']").val(),
+				}, function(data, status){
+					if (status == "success") {
+						if (!data.isSuccess) {
+							$("#forgetForm").prepend(getAlertTips("danger", data.tips));
+							return;
+						}
+						// 创建登录成功的弹窗
+						createIntervalDialog("<h2>密码更新成功!</h2>", 2);
 					} else {
 						alert("注册失败！")
 					}
