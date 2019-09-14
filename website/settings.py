153ca48b792ffccb5ea889cb4e9aabeddc035b6a
JimDreamHeart <15602291936@163.com>
2019-09-14 13:35:19 +0800

    refactor:更新配置

--- website/settings.py
+++ website/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pytoolsip',
         'USER': 'root',
-        'PASSWORD': 'pwdis123456',
+        'PASSWORD': 'Pwd@123456',
         'HOST':'localhost',
         'PORT':'3306',
         'OPTIONS':{
